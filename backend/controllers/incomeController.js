bb39372c5f10407ff56b577943f61b5f7ca001f3
Patrick Sheridan <sheridp2@gmail.com>
2025-07-14 15:13:48 -0700

    Image uploader and income routes added

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,33 @@
+const User = require("../models/User");
+const Income = require("../models/Income");
+
+exports.addIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const { icon, source, amount, date } = req.body;
+
+    if (!source || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getAllIncome = async (req, res) => {};
+
+exports.deleteIncome = async (req, res) => {};
+
+exports.downloadIncomeExcel = async (req, res) => {};
