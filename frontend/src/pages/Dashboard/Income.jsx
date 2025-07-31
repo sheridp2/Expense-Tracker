8c7603be96b90066c15389444bfd6c6b3eca49ab
Patrick Sheridan <sheridp2@gmail.com>
2025-07-31 16:24:54 -0700

    Expense and income excel downloads working

--- frontend/src/pages/Dashboard/Income.jsx
+++ frontend/src/pages/Dashboard/Income.jsx
@@ -92,7 +92,28 @@ export default function Income() {
     }
   };
 
-  const handleDownloadIncomeDetails = async () => {};
+  
+  const handleDownloadIncomeDetails = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.INCOME.DOWNLOAD_INCOME,
+        {
+          responseType: "blob",
+        }
+      );
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "income_details.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading income details", error)
+      toast.error("Failed to download income details. Please try again")
+    }
+  };
 
   useEffect(() => {
     fetchIncomeDetails();
@@ -115,7 +136,7 @@ export default function Income() {
             onDelete={(id, source) => {
               setOpenDeleteAlert({ show: true, data: id, source: source });
             }}
-            onDOwnload={handleDownloadIncomeDetails}
+            onDownload={handleDownloadIncomeDetails}
           />
         </div>
 
